8bd998f3d4bc3b70deb48423d7388f5d16c07422
alexdent42 <manhappen@gmail.com>
2022-03-14 23:18:30 +0300

    top charts finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './navbar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import FormatListBulletedRoundedIcon from '@mui/icons-material/FormatListBulletedRounded';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search...' />
+          <SearchIcon/>
+        </div>
+          <div className="items">
+            <div className="item">
+              <LanguageIcon className="icon"/>
+              English
+            </div>
+            <div className="item">
+              <NightlightIcon className="icon"/>
+            </div>
+            <div className="item">
+              <FullscreenExitIcon className="icon"/>
+              </div>
+              <div className="item">
+              <NotificationsNoneIcon className="icon"/>
+              <div className="counter">1</div>
+
+              </div>
+              <div className="item">
+              <ChatBubbleOutlineIcon className="icon"/>
+              <div className="counter">23</div>
+              </div>
+              <div className="item">
+              <FormatListBulletedRoundedIcon className="icon"/>
+              </div>
+              <div className="item">
+              <img src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg" alt="" className='avatar'/>
+              </div>
+          </div>
+
+      </div>
+    </div>
   )
 }
 
