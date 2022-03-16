44a6aea76adce1ac59f295850ab47b70f28c2820
alexdent42 <manhappen@gmail.com>
2022-03-16 17:26:25 +0300

    datatable --page-list finished

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 import './list.css'
+import Datatable from '../../components/datatable/Datatable'
+
 
 const List = () => {
   return (
-    <div>List</div>
+    <div className="list">
+      <Sidebar/>
+      <div className="listCont">
+        <Navbar />
+        <Datatable/>
+      
+      </div>
+    
+    </div>
   )
 }
 
