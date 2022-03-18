db14e79116e0c9b7e89cbdd2d24b8b3e52ed1842
alexdent42 <manhappen@gmail.com>
2022-03-18 17:28:59 +0300

    formSource.jsx finished

---
+++ src/formSource.jsx
@@ -0,0 +1,77 @@
+
+export const userInputs = [
+    {
+        id:1,
+        label: "Username",
+        type: "text",
+        placeholder: "John Doe",
+    },
+    {
+        id:2,
+        label: "Name and Surname",
+        type: "text",
+        placeholder: "John Doe",
+    },
+    {
+        id:3,
+        label:"Email",
+        type:"email",
+        placeholder:"john.doe@gmail.com",
+    },
+    {
+        id:4,
+        label:"Phone",
+        type:"text",
+        placeholder:"+473894739804",
+    },
+    {
+        id:5,
+        label: "Password",
+        type:"Password",
+    },
+    {
+        id:6,
+        label:"Address",
+        type:"text",
+        placeholder:"Elton St.216 New York",
+    },
+    {
+        id:7,
+        label:"Country",
+        type:"text",
+        placeholder:"USA",
+    },
+];
+
+export const productsInputs = [
+    {
+        id:1,
+        label:"Title",
+        type:"text",
+        placeholder:"Apple MacBook Pro",
+    },
+    {
+        id:2,
+        label:"Desciption",
+        type:"text",
+        placeholder:"Description",
+    },
+    {
+        id:3,
+        label:"Category",
+        type:"text",
+        placeholder:"Computers",
+    },
+    {
+        id:4,
+        label:"Price",
+        type:"text",
+        placeholder:"100",
+    },
+    {
+        id:5,
+        label:"Stock",
+        type:"text",
+        placeholder:"in stock",
+    },
+];
